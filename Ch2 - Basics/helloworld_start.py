a91bb1eab8bcd3484854709aa2046f2fdfe80fd2
Vishnu Sah <127580123+vishnuSah@users.noreply.github.com>
2024-01-23 10:06:18 +0000

    helloworld

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,13 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print("Hello World !")
+    name = input("What is your name ?")
+    print("Nice to meet you ",name)
 
+
+
+if __name__ == "__main__":
+    main()
+    
