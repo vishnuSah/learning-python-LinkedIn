4ff8cb8e33943ce43080930e3597b9bfbcacbd1a
Vishnu Sah <127580123+vishnuSah@users.noreply.github.com>
2024-01-23 14:05:43 +0000

    functions

--- Ch2 - Basics/functions_start.py
+++ Ch2 - Basics/functions_start.py
@@ -5,17 +5,42 @@
 
 
 # TODO: define a basic function
-
+def function():
+    print("I am Function")
 
 # TODO: function that takes arguments
-
+def function2(x,y):
+    print(x," ", y)
 
 # TODO: function that returns a value
-
+def cube(x):
+    return x*x*x 
 
 # TODO: function with default value for an argument
-
+def function3(arg, x=1):
+    result = 1
+    for _ in range(x):
+        result = result * arg
+    return result 
 
 # TODO: function with variable number of arguments
+def function4(*args):
+    result = 0
+    for x in args:
+        result = result + x
+    return result 
 
 
+# function()
+# print(function())
+# print(function)
+
+# function2(2,3)
+# print(function2(2,3))
+
+#cube(3)
+#print(cube(3))
+
+#print(function3(2,3))
+
+print(function4(2,3,4,5, 10))
