8541fab0abf6aa9d0b576f8c1b58f94f3c2fae64
Vishnu Sah <127580123+vishnuSah@users.noreply.github.com>
2024-01-23 10:29:04 +0000

    variables

--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -13,25 +13,43 @@ mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
+# print(myint)
+# print(myfloat)
 print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+# print(mybool)
+# print(mylist)
+# print(mytuple)
+# print(mydict)
 
 # re-declaring a variable works
+# myint = "stringVariable"
+# print(myint)
 
 # to access a member of a sequence type, use []
+# print(mylist[2])
+# print(mytuple[2])
+# # use slices to get parts of a sequence
+# print(mylist[1:5])
+# print(mytuple[1:5])
 
-# use slices to get parts of a sequence
+# # you can use slices to reverse a sequence
+# print(mylist[::-1])
 
-# you can use slices to reverse a sequence
+# # dictionaries are accessed via keys
+# print(mydict['two'])
 
-# dictionaries are accessed via keys
+# # ERROR: variables of different types cannot be combined
+# #mystr = "This is a string"
+# print(mystr + str(5))
 
-# ERROR: variables of different types cannot be combined
 
 # Global vs. local variables in functions
 
+def someFunction():
+    global mystr
+    mystr = "local variable"
+    print(mystr)
+
+someFunction()
+del mystr
+print(mystr)
