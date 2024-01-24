ca988ce7949e3b3843f726bb5230183d9706f9f2
Vishnu Sah <127580123+vishnuSah@users.noreply.github.com>
2024-01-24 13:43:02 +0000

    loops and classes

--- Ch2 - Basics/classes_start.py
+++ Ch2 - Basics/classes_start.py
@@ -3,3 +3,50 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+class Vehicle():
+    def __init__(self, bodystype):
+        self.bodytype = bodystype 
+
+    def drive(self, speed):
+        self.mode = "Driving"
+        self.speed = speed
+
+class Car(Vehicle):
+    def __init__(self, enginetype):
+        super().__init__("Car")
+        self.wheel = 4
+        self.door = 4
+        self.enginetype = enginetype
+    
+    def drive(self,speed):
+        super().drive(speed)
+        print(self.mode,"my", self.enginetype, "car at", self.speed, "speed")
+
+class Motorcycle(Vehicle):
+    def __init__(self, enginetype, sideCar):
+        super().__init__("Motocycle")
+        self.enginetype = enginetype
+        self.sideCar = sideCar
+
+        if (sideCar):
+            self.wheel = 3
+        else:
+            self.wheel = 2
+
+    def drive(self, speed):
+        super().drive(speed)
+        print(self.mode, self.enginetype, "motorbike at" , self.speed, "speed")
+
+
+car1 = Car('gas')
+car2 = Car("electric")
+mc = Motorcycle('gas', True)
+
+
+print(car1.enginetype)
+print(car2.door)
+print(mc.enginetype)
+print(mc.wheel)
+
+print(car1.drive(50))
+print(mc.drive(90))
--- Ch2 - Basics/loops_start.py
+++ Ch2 - Basics/loops_start.py
@@ -8,20 +8,28 @@ def main():
     x = 0
 
     # TODO: define a while loop
-
+    # while (x < 5):
+    #     x = x + 1
+    #     print(x)
 
     # TODO: define a for loop
-
+    # for x in range(5,10):
+    #     if x == 8:
+    #         continue
+    #     print(x)
 
     # TODO: use a for loop over a collection
     days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    # for day in days:
+    #     print(day)
 
 
     # TODO: use the break and continue statements
 
 
     # TODO: using the enumerate() function to get index 
-    
+    for i,day in enumerate(days):
+        print(i,day)
   
 if __name__ == "__main__":
     main()
